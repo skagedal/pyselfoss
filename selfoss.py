6789f1336544a310aebabf8f402796ddfbd2fc1c
Simon Kågedal Reimer <skagedal@gmail.com>
2013-05-16 22:29:50 +0200

    Python 3, args to items, __repr__, add item fields
    - Support Python 3 (and Python 2 simultaneously)
    - Give Item a __repr__
    - Let items() take optional arguments
    - Parse the data fields a little or just copy to Item class

--- selfoss.py
+++ selfoss.py
@@ -1,14 +1,39 @@
-import httplib
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys
 import urllib
 import json
+import datetime
+if sys.version_info[0] >= 3:
+    import http.client
+    httplib = http.client
+    urlencode = urllib.parse.urlencode
+else:
+    import httplib
+    urlencode = urllib.urlencode
+
 
 class Item:
     def __init__(self, selfoss, data):
         self.selfoss = selfoss
         self.data = data
+
         self.id = data['id']
+        self.datetime = datetime.datetime.strptime(data['datetime'], "%Y-%m-%d %H:%M:%S")
+        self.title = data['title']
+        self.content = data['content']
         self.unread = data['unread'] == u'1'
         self.starred = data['starred'] == u'1'
+        self.source = data['source']
+        self.thumbnail = data['thumbnail']
+        self.icon = data['icon']
+        self.uid = data['uid']
+        self.link = data['link']
+        self.sourcetitle = data['sourcetitle']
+        self.tags = data['tags']
+
+    def __repr__(self):
+        return "<Item %s: %s>" % (self.id, self.title)
 
     def mark(self):
         ret = self.selfoss.mark(self.id)
@@ -27,12 +52,12 @@ class Selfoss:
 
     def request(self, method, command, params = {}):
         data = None
-        params = urllib.urlencode(params)
+        params = urlencode(params)
         conn = httplib.HTTPConnection(self.server)
         conn.request(method, self.base + command, params)
         res = conn.getresponse()
         if res.status == 200:
-            data = res.read()
+            data = res.read().decode('utf-8')
 
         conn.close()
         if data is not None:
@@ -50,9 +75,11 @@ class Selfoss:
         return self.GET("/sources/list")
 
     def items(self, **kwargs):
-        """List items. Optional keyword arguments:
-           """
-        return [Item(self, data) for data in self.GET("/items")]
+        params = {}
+        for key in ["type", "search", "tag", "source", "offset", "items"]:
+            if key in kwargs:
+                params[key] = kwargs[key]
+        return [Item(self, data) for data in self.GET("/items", params)]
 
     def mark(self, item_id):
         return self.POST("/mark/%s" % item_id)
@@ -61,3 +88,4 @@ class Selfoss:
         return self.POST("/unmark/%s" % item_id)
 
     
+local = Selfoss('localhost', '/simon/selfoss')
